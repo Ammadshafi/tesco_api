23a3c1acfdd616417f9544246df11dd8a62e21a8
unknown <kigucha1234567@gmail.com>
2023-01-05 13:28:31 +0545

    staged changed

--- server.js
+++ server.js
@@ -5,12 +5,19 @@ const TeacherRoute=require('./route/Teacher_route')
 const swaggerUi = require('swagger-ui-express');
 const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerDefinition=require('./swagger.doc')
+const cors = require('cors');
 const app= express()
-const PORT=8000
+const PORT=5000
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 //Here is the APi of the different routing
 app.use('/api',TeacherRoute)
+app.use(cors())
 
 db.connection()
 
